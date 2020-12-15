87099ecac9ec91db75373a876fa4c9da7922c0b4
Suzon Das <suzonduislm52@gmail.com>
2020-12-15 16:20:56 +0600

    added VUE bootstrap markup

---
+++ site/resources/views/entry.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+
+    <link href="css/app.css" rel="stylesheet">
+</head>
+<body>
+<div class="container" id="app">
+<div class="text-xl-center">Primary PEC Form</div>
+</div>
+
+<script src="js/app.js" defer></script>
+</body>
+</html>
