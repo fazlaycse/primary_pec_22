199b5e5d0a52636c1553497c60979dc8470b869e
Suzon Das <suzonduislm52@gmail.com>
2021-01-07 18:01:19 +0600

    added VUEJS

--- site/app/Http/Controllers/InstituteController.php
+++ site/app/Http/Controllers/InstituteController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class InstituteController extends Controller
 {
-    //
+    public function index(){
+
+    }
+    public  function create(){
+
+    }
+    public function read(){
+
+    }
+    public function update(){
+
+    }
+    public function delete(){
+
+    }
 }
