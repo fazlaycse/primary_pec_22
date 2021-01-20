98866657ee655c485cf48173a756e998268c85e5
Suzon Das <suzonduislm52@gmail.com>
2021-01-20 23:02:07 +0600

    fields fixed

--- site/app/Http/Controllers/TeacherInfosController.php
+++ site/app/Http/Controllers/TeacherInfosController.php
@@ -47,7 +47,7 @@ class TeacherInfosController extends Controller
                 $request->session()->forget('institute_id');
                 return response('OK', 200);
             } catch (\Exception $e) {
-                return response(json(['error' =>  $e->getMessage()]), 401);
+                return response(json_encode(['error' =>  $e->getMessage()]), 401);
 
             }
         }
--- site/resources/views/teacher_info.blade.php
+++ site/resources/views/teacher_info.blade.php
@@ -6,7 +6,7 @@
     <link href="css/app.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
     <script>
-        window.teacherData = "{{$instRow}}";
+        window.teacherData = "<?php echo $instRow; ?>";
     </script>
 </head>
 <body>
