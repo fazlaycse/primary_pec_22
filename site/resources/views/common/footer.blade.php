06139466eebe5be60e667b4b301fd8052eeda87b
Suzon Das <suzonduislm52@gmail.com>
2021-01-21 13:57:59 +0600

    include pages added in view/common/

---
+++ site/resources/views/common/footer.blade.php
@@ -0,0 +1,11 @@
+<hr>
+<div class="footer">
+    <div class="row footer">
+        <div class="col-md-12"
+             style="text-align:center; padding:5px; font-family: 'Lucida Sans'; background-color: #a1cbef;">
+            <span> &copy; Powered by ICT Division, BANBEIS</span> <br>
+            <span> Ministry of Education</span>
+        </div>
+    </div>
+</div>
+<hr>
