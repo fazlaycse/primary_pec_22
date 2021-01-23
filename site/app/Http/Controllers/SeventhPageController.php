0e55a0e310ec539f1bd269557088b42cd03e0013
Saad <mdliazmahamud@gmail.com>
2021-01-23 15:52:50 +0600

    seventhpageController save by lemon

--- site/app/Http/Controllers/SeventhPageController.php
+++ site/app/Http/Controllers/SeventhPageController.php
@@ -23,80 +23,6 @@ class SeventhPageController extends Controller
               exit();*/
 
         $instRow = [];
-        $buildingId='';
-        //deal with Buildings Table
-        try {
-            $row = Buildings::where('institute_id', '=', $request->session()->get('institute_id'))->firstOrFail();
-            $buildingId=$row->id;
-            array_push($instRow, $row);
-        } catch (\Exception $e) {
-            // do task when error
-            $row = new Buildings();
-            //$row->year = 2020;
-            $row->institute_id = $request->session()->get('institute_id');
-            try {
-                $row->save(); // returns false
-                $buildingId=$row->id;
-                array_push($instRow, $row);
-            } catch (\Exception $e) {
-                echo $e;
-            }
-        }
-
-        //deal with Building_infos Table
-        try {
-
-            $row = Building_infos::where('institute_id', '=', $request->session()->get('institute_id'))->firstOrFail();
-            //print_r($row); exit;
-            array_push($instRow, $row);
-        } catch (\Exception $e) {
-            // do task when error
-            $row = new Building_infos();
-            $row->year = 2020;
-            $row->building_id = $buildingId;
-            $row->institute_id = $request->session()->get('institute_id');
-            try {
-                $row->save(); // returns false
-                array_push($instRow, $row);
-            } catch (\Exception $e) {
-                echo $e;
-            }
-        }
-
-        //deal with Institute_sanitations  Table
-        try {
-            $row = Institute_sanitations::where('institute_id', '=', $request->session()->get('institute_id'))->firstOrFail();
-            array_push($instRow, $row);
-        } catch (\Exception $e) {
-            // do task when error
-            $row = new Institute_sanitations();
-            $row->year = 2020;
-            $row->institute_id = $request->session()->get('institute_id');
-            try {
-                $row->save(); // returns false
-                array_push($instRow, $row);
-            } catch (\Exception $e) {
-                echo $e;
-            }
-        }
-
-        //deal with Washblocks  Table
-        try {
-            $row = Washblocks::where('institute_id', '=', $request->session()->get('institute_id'))->firstOrFail();
-            array_push($instRow, $row);
-        } catch (\Exception $e) {
-            // do task when error
-            $row = new Washblocks();
-            $row->year = 2020;
-            $row->institute_id = $request->session()->get('institute_id');
-            try {
-                $row->save(); // returns false
-                array_push($instRow, $row);
-            } catch (\Exception $e) {
-                echo $e;
-            }
-        }
-
         //deal with Water_facilities  Table
         try {
             $row = Water_facilities::where('institute_id', '=', $request->session()->get('institute_id'))->firstOrFail();
@@ -130,14 +56,9 @@ class SeventhPageController extends Controller
                 echo $e;
             }
         }
-        //echo $buildingId;die;
         $instRowObj = new \stdClass();
-        $instRowObj->buildings = $instRow[0];
-        $instRowObj->building_infos = $instRow[1];
-        $instRowObj->institute_sanitations = $instRow[2];
-        $instRowObj->washblocks = $instRow[3];
-        $instRowObj->water_facilities = $instRow[4];
-        $instRowObj->ict_multimedias = $instRow[5];
+        $instRowObj->water_facilities = $instRow[0];
+        $instRowObj->ict_multimedias = $instRow[1];
         return view('seventh_page')->with('instRowObj',$instRowObj);
 
     }
@@ -146,56 +67,6 @@ class SeventhPageController extends Controller
     {
         if ($request->method() == 'POST') {
             //var_dump($request->all()); exit;
-            try {
-                $updateOrcreateRow = Buildings::updateOrCreate(['institute_id' => $request->session()->get('institute_id')], $request->all());
-                $buildingId=$updateOrcreateRow->id;
-            } catch (\Exception $e) {
-                echo $e->getMessage();
-
-                // do task when error
-                /*echo $e->getMessage();
-                exit();*/
-                Session::flash('message', 'Something went wrong for Buildings Table!');
-                return redirect('/third_page');
-            }
-
-            try {
-
-                $updateOrcreateRow = Building_infos::updateOrCreate(['institute_id' => $request->session()->get('institute_id')], $request->all());
-            } catch (\Exception $e) {
-                echo $e->getMessage();
-
-                // do task when error
-                /*echo $e->getMessage();
-                exit();*/
-                Session::flash('message', 'Something went wrong for Building_infos Table!');
-                return redirect('/third_page');
-            }
-
-            try {
-                $updateOrcreateRow = Institute_sanitations::updateOrCreate(['institute_id' => $request->session()->get('institute_id')], $request->all());
-            } catch (\Exception $e) {
-                echo $e->getMessage();
-
-                // do task when error
-                /*echo $e->getMessage();
-                exit();*/
-                Session::flash('message', 'Something went wrong for Institute_sanitations Table!');
-                return redirect('/third_page');
-            }
-
-            try {
-                $updateOrcreateRow = Washblocks::updateOrCreate(['institute_id' => $request->session()->get('institute_id')], $request->all());
-                //print_r($request->all());die;
-            } catch (\Exception $e) {
-                echo $e->getMessage();
-
-                // do task when error
-                /*echo $e->getMessage();
-                exit();*/
-                Session::flash('message', 'Something went wrong for Washblocks Table!');
-                return redirect('/third_page');
-            }
 
             try {
                 $updateOrcreateRow = Water_facilities::updateOrCreate(['institute_id' => $request->session()->get('institute_id')], $request->all());
@@ -206,7 +77,7 @@ class SeventhPageController extends Controller
                 /*echo $e->getMessage();
                 exit();*/
                 Session::flash('message', 'Something went wrong for Water_facilities Table!');
-                return redirect('/third_page');
+                return redirect('/sixth_page');
             }
 
             try {
@@ -218,7 +89,7 @@ class SeventhPageController extends Controller
                 /*echo $e->getMessage();
                 exit();*/
                 Session::flash('message', 'Something went wrong for Ict_multimedias Table!');
-                return redirect('/third_page');
+                return redirect('/sixth_page');
             }
 
             Session::flash('message', 'Data Saved Successfully!');
