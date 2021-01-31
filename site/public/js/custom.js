e622af4632e90f5e25892ba98821ae09723937ef
Suzon Das <suzonduislm52@gmail.com>
2021-01-31 17:06:22 +0600

    js redirect static page

--- site/public/js/custom.js
+++ site/public/js/custom.js
@@ -90,7 +90,7 @@ var app = new Vue({
             Vue.http.headers.common['X-CSRF-TOKEN'] = $('#token').val();
             this.$http.post('teacher_info_page_save', this.rowData)
                 .then(function (response) {
-                    window.location.href = '/thanking_page';
+                    window.location.href = 'thanking_page';
                 })
                 .catch(function (error) {
                     console.log(error)
